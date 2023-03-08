ee35e9d6499e02c951137f2e5b6b4fdef1082865
antonstrodthoff <anton.strodthoff@googlemail.com>
2023-03-08 16:52:04 +0100

    Added indexToDate function

--- plotter.py
+++ plotter.py
@@ -2,6 +2,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+def indexToDate(index):
+    return pd.to_datetime(index, unit="D", origin=pd.Timestamp("1962.01.01"))
+
+print(indexToDate(1).strftime("%Y.%m.%d"))
+
 csv = pd.read_csv("task_student/bbdc_2023_AWI_data_develop_student.csv", sep=";", na_values=["NA", "NaN", None, np.nan])
 csv.fillna(np.nan, inplace=True)
 csv = csv.drop(0)
@@ -12,6 +17,6 @@ datalength = csv.shape[0]
 xx = np.arange(0, datalength - 1, 1)
 yy = csv.iloc[1:, 3].interpolate(method="linear", limit_direction="both")
 
-plt.plot(xx, yy)
-plt.show()
+#plt.plot(xx, yy)
+#plt.show()
 
