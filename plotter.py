5881c4ccb8780ede24a324eda3497f529ded2464
antonstrodthoff <anton.strodthoff@googlemail.com>
2023-03-09 16:20:15 +0100

    Cleaned up Kot

--- plotter.py
+++ plotter.py
@@ -10,10 +10,6 @@ from upload_scoring import score_all
 def indexToDate(index):
     return pd.to_datetime(index, unit="D", origin=pd.Timestamp("1962.01.01"))
 
-def avg(dataSeries, N):
-    meanSeries = dataSeries.rolling(window=N, center=True).mean()
-    return meanSeries
-
 def writeData(data, dataLength):
     results = pd.read_csv("task_student/bbdc_2023_AWI_data_evaluate_skeleton_student.csv", sep=";")
     resultLength = results.shape[0]
@@ -33,17 +29,14 @@ data = pd.read_csv("task_student/bbdc_2023_AWI_data_develop_student.csv", sep=";
 data.fillna(np.nan, inplace=True)
 data = data.drop(0)
 data[["SECCI", "Temperatur", "Salinität", "NO2", "NO3", "NOx"]] = data[["SECCI", "Temperatur", "Salinität", "NO2", "NO3", "NOx"]].astype(float)
+
 interpolatedData = data.copy()
-rollingMeanData = data.copy()
+interpolatedData = interpolatedData.interpolate(method="linear", limit_direction="both")
+    
+rollingMeanData = interpolatedData.copy()
+rollingMeanData.iloc[:, 2:7] = rollingMeanData.iloc[:, 2:7].rolling(window=50, center=True).mean()
 
 dataLength = data.shape[0]
-
-for i in range(2,7):
-    #writes interpolated values into the dataframe
-    interpolatedData.iloc[:, i] = interpolatedData.iloc[:, i].interpolate(method="linear", limit_direction="both")
-    #writes averaged values into the dataframe
-    rollingMeanData.iloc[:, i] = avg(rollingMeanData.iloc[:, i], 50)
-
 xx = np.arange(0, dataLength, 1)
 
 plt.plot(xx, data.iloc[:, plotColumn], linewidth=1, alpha=1)
@@ -51,12 +44,13 @@ plt.plot(xx, interpolatedData.iloc[:, plotColumn], linewidth=1, alpha=0.3)
 plt.plot(xx, rollingMeanData.iloc[:, plotColumn], linewidth=1, alpha=0.3)
 plt.ylabel(data.columns[plotColumn])
 plt.legend(["Original", "Interpolated", "Averaged"])
-plt.show()
+#plt.show()
 
-data.fillna(0, inplace=True)
+print(rollingMeanData.shape)
+print(rollingMeanData.head(60))
 print("The score of the year 2009 compared to the year 2010 is:")
-print(score_all(data.iloc[17166:17531, 2:7], data.iloc[16801:17166, 2:7]))
-print("(All np.nan values are replaced with 0.0)")
+print(score_all(interpolatedData.iloc[17166:17531, 2:7], interpolatedData.iloc[16801:17166, 2:7]))
+print(score_all(rollingMeanData.iloc[17166:17531, 2:7], rollingMeanData.iloc[16801:17166, 2:7]))
 
 #writeData(data, dataLength)
 
