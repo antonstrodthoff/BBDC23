b68fe2235f8235371cf02eb7a0b524c99f654850
antonstrodthoff <anton.strodthoff@googlemail.com>
2023-03-10 12:44:49 +0100

    Compare graphs

--- plotCSV.py
+++ plotCSV.py
@@ -1,22 +1,27 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import datetime as dt
 
-def plotColumn(fileName="", plotColumn=2, separator=";", dropFirstNValues=1, label=""):
-    data = pd.read_csv(fileName, sep=separator, na_values=["NA", "NaN", None, np.nan])
+def plotColumn(fileName="", plotColumn=2, separator=";", dropFirstNValues=1, dateFormat="%d.%m.%Y", label=""):
+    data = pd.read_csv(fileName, sep=separator, na_values=["NA", "NaN", None, np.nan, ""], on_bad_lines="skip")
     data.fillna(np.nan, inplace=True)
     data = data.drop([i for i in range(0, dropFirstNValues)], axis=0)
     data.iloc[:, plotColumn] = data.iloc[:, plotColumn].replace(0, np.nan).astype(float)
 
     dataLength = data.shape[0]
-    xx = np.arange(0, dataLength, 1)
+    xx = [dt.datetime.strptime(d, dateFormat).date() for d in data.iloc[:, 0]]
 
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%m.%Y"))
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=365*5))
     plt.plot(xx, data.iloc[:, plotColumn], linewidth=1, alpha=1, label=label)
+    plt.gcf().autofmt_xdate()
 
 #plot1
-plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=2, separator=";", dropFirstNValues=1, label="training")
+plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=3, separator=";", dropFirstNValues=1, label="training")
 #plot2
-plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=2, separator=";", dropFirstNValues=1, label="training2")
+plotColumn(fileName="research_data/List_Reede.csv", plotColumn=3, separator=",", dropFirstNValues=1, dateFormat="%Y-%m-%dT%H:%M", label="reasearched")
 
 plt.legend()
 plt.show()
