af27c76f3f3835b6988fdd7df5a31fcfd2859612
antonstrodthoff <anton.strodthoff@googlemail.com>
2023-03-10 16:35:15 +0100

    save column function

--- plotCSV.py
+++ plotCSV.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime as dt
+import interpolater as ip
 
 def plotColumn(fileName="", plotColumn=2, separator=";", dropFirstNValues=1, dateFormat="%d.%m.%Y", label="", window=0):
     data = pd.read_csv(fileName, sep=separator, na_values=["NA", "NaN", None, np.nan, ""], on_bad_lines="skip")
@@ -13,6 +14,9 @@ def plotColumn(fileName="", plotColumn=2, separator=";", dropFirstNValues=1, dat
     else:
         data.iloc[:, plotColumn] = data.iloc[:, plotColumn].replace(0, np.nan).astype(float).rolling(window=window, min_periods=window//2).mean()
 
+    print(data.head(10))
+    ip.writeColumnToResultFile(data, plotColumn, data.shape[0])
+
     dataLength = data.shape[0]
     xx = [dt.datetime.strptime(d, dateFormat).date() for d in data.iloc[:, 0]]
 
@@ -22,10 +26,19 @@ def plotColumn(fileName="", plotColumn=2, separator=";", dropFirstNValues=1, dat
     plt.gcf().autofmt_xdate()
 
 #plot1
-plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=3, separator=";", dropFirstNValues=1, label="training")
+plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=2, separator=";", dropFirstNValues=1, label="training")
 #plot2
-plotColumn(fileName="research_data/List_Reede.csv", plotColumn=3, separator=",", dropFirstNValues=1, dateFormat="%Y-%m-%dT%H:%M", label="reasearched", window=10)
+#plotColumn(fileName="research_data/List_Reede.csv", plotColumn=3, separator=",", dropFirstNValues=1, dateFormat="%Y-%m-%dT%H:%M", label="reasearched", window=10)
 
-plt.legend()
-plt.show()
+#plt.legend()
+#plt.show()
+
+#test = pd.DataFrame(np.array([["01.01.2000", "02.01.2000", "03.01.2000"], [1,2,3]]).T)
+#test = test.where(test.iloc[:, 0].astype(str) == "01.01.2000", np.nan, inplace=False).dropna(how="all")
+#print(test)
+
+#data = pd.read_csv("task_student/bbdc_2023_AWI_data_develop_student.csv", sep=";", na_values=["NA", "NaN", None, np.nan, ""])
+#print(pd.to_datetime(data.iloc[:, 0]).info())
+#data = data.where(data.iloc[:, 0].astype(str) == "01.01.2000", np.nan, inplace=False).dropna(how="all")
+#print(data)
 
