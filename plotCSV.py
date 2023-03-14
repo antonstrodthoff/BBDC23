fd4630126a17144c6b644140d990181f80e6d04a
antonstrodthoff <anton.strodthoff@googlemail.com>
2023-03-14 08:57:58 +0100

    Up to date

--- plotCSV.py
+++ plotCSV.py
@@ -5,14 +5,14 @@ import matplotlib.dates as mdates
 import datetime as dt
 #import interpolater as ip
 
-def plotColumn(fileName="", plotColumn=2, separator=";", dropFirstNValues=1, dateFormat="%d.%m.%Y", label="", window=0, alpha=0.3):
+def plotColumn(fileName="", plotColumn=2, separator=";", dropFirstNValues=1, dateFormat="%d.%m.%Y", label="", window=0, alpha=1):
     data = pd.read_csv(fileName, sep=separator, na_values=["NA", "NaN", None, np.nan, ""], on_bad_lines="skip")
     data.fillna(np.nan, inplace=True)
     data = data.drop([i for i in range(0, dropFirstNValues)], axis=0)
     if window == 0:
         data.iloc[:, plotColumn] = data.iloc[:, plotColumn].replace(0, np.nan).astype(float)
     else:
-        data.iloc[:, plotColumn] = data.iloc[:, plotColumn].replace(0, np.nan).astype(float).rolling(window=window, min_periods=window//2).mean()
+        data.iloc[:, plotColumn] = data.iloc[:, plotColumn].replace(0, np.nan).astype(float).interpolate(limit_direction="both").rolling(window=window, min_periods=window//2).mean()
 
     #ip.writeColumnToResultFile(data, plotColumn, data.shape[0])
 
@@ -34,15 +34,16 @@ def plotSecciOverTime():
 
 
 #plot training columns
-#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=2, separator=";", dropFirstNValues=1, label="t_secci", window=50)
-plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=3, separator=";", dropFirstNValues=1, label="t_temp", window=50)
-#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=4, separator=";", dropFirstNValues=1, label="t_salinity", window=50)
-#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=5, separator=";", dropFirstNValues=1, label="t_no2", window=50)
-#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=6, separator=";", dropFirstNValues=1, label="t_no3", window=50)
-#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=7, separator=";", dropFirstNValues=1, label="t_nox", window=50)
+#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=2, separator=";", dropFirstNValues=1, label="t_secci", window=0)
+#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=3, separator=";", dropFirstNValues=1, label="t_temp", window=0)
+#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=4, separator=";", dropFirstNValues=1, label="t_salinity", window=0)
+#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=5, separator=";", dropFirstNValues=1, label="t_no2", window=0)
+plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=6, separator=";", dropFirstNValues=1, label="t_no3", window=50)
+#plotColumn(fileName="task_student/bbdc_2023_AWI_data_develop_student.csv", plotColumn=7, separator=";", dropFirstNValues=1, label="t_nox", window=0)
 
 #plot researched columns
-plotColumn(fileName="research_data/List_Reede.csv", plotColumn=3, separator=",", dropFirstNValues=1, dateFormat="%Y-%m-%dT%H:%M", label="r_salinity", window=10, alpha=1)
+#plotColumn(fileName="research_data/List_Reede.csv", plotColumn=3, separator=",", dropFirstNValues=1, dateFormat="%Y-%m-%dT%H:%M", label="r_salinity", window=10, alpha=1)
+plotColumn(fileName="research_data/List_Reede.csv", plotColumn=11, separator=",", dropFirstNValues=1, dateFormat="%Y-%m-%dT%H:%M", label="r_no3", window=50, alpha=1)
 
 #plotSecciOverTime()
 
